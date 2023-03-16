67b212a918df6ad4036f2a040a2e1af58d784f3e
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-03-16 16:02:14 +0300

    addded new changes

--- factorial.js
+++ factorial.js
@@ -5,7 +5,7 @@ const factorialCard=`<div class="container">
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col-6">
-                                        <input id="numberInput" type="text" class="form-control" placeholder="Enter a number to find factorial"/>
+                                        <textarea id="numberInput" row="3" class="form-control" placeholder="Enter a number to find factorial"></textarea>
                                     </div>
                                     <div class="col-6 bg-dark">
                                         <h4 id="factorialResults" class="text-white text-center"></h4>
@@ -33,7 +33,17 @@ function factorial(num){
 }
 
 document.getElementById("computeFactorialBtn").addEventListener("click",()=>{
-    const number=parseInt(document.getElementById("numberInput").value) ;
+        let  number=document.getElementById("numberInput").value;
+    if(number===""){
+        alert("Please enter a number or a Valid number to find factorial");
+    }else{
+        number =parseInt(number)
+        if (number===NaN){
+            alert("Please enter a valid number to find factorial");
+        }else{
+
     const factorialResults= factorial(number);
     document.getElementById("factorialResults").innerHTML=factorialResults;
+        }
+        }
 })
